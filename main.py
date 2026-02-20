5e2285fc555c41756d94ebfd5e72b2bd2b01443b
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-02-20 11:53:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ print("\n ")
 #Declaração de variáveis
 
 n_secreto = 68
-entrada = int(input("Digite um número: "))
-acerto = entrada ==  n_secreto
-numero_maior = entrada > n_secreto
-numero_menor = entrada < n_secreto
+tentativas = 3
+rodada = 1
 
-#Digitar o número
-print(f"Você digitou o número: {entrada}")
+for rodada in range(1,tentativas +1): 
+    print(f"Tentativa {rodada} de {tentativas}")
+    entrada = int(input("Digite um número: "))
+    acerto = entrada ==  n_secreto
+    numero_maior = entrada > n_secreto
+    numero_menor = entrada < n_secreto
 
-#Condicional
-if (acerto):
-    print("PARABÉNS!!!! Você acertou o número secreto")
-    
-else:
-    if(numero_maior):
-        print("O número digitado foi MAIOR do que o número secreto")
-    if(numero_menor):
-        print("O número digitado foi MENOR do que o número secreto")
+    #Digitar o número
+    print(f"Você digitou o número: {entrada}")
 
-
-print("GAME OVER")
+    #Condicional de acerto ou erro
+    if (acerto):
+        print("PARABÉNS!!!! Você acertou o número secreto")
+        break
+    else:
+        if(numero_maior):
+            print("O número digitado foi MAIOR do que o número secreto")
+        if(numero_menor):
+            print("O número digitado foi MENOR do que o número secreto")
+    rodada = rodada + 1
+print("GAME OVER!!!")
