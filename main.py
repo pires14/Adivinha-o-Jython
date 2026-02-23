374da3ffba491a9c1b96fcd6f72527a6e50c7e5c
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-02-23 11:50:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+import random as rd
+
 print("\n ")
 print("\n Bem vindo ao jogo de adivinhação!")
 print("\n ")
 
 #Declaração de variáveis
-
-n_secreto = 68
-tentativas = 3
+n_secreto = rd.randrange(1,101)
+tentativas = 10
 rodada = 1
 
 for rodada in range(1,tentativas +1): 
     print(f"Tentativa {rodada} de {tentativas}")
-    entrada = int(input("Digite um número: "))
+    entrada = int(input("Digite um número de 1 a 100: "))  
+    if (entrada >  100 or entrada < 1): 
+        print("VOCÊ NÃO SABE LER???? O valor digitado não é permitido")
+        continue
     acerto = entrada ==  n_secreto
     numero_maior = entrada > n_secreto
     numero_menor = entrada < n_secreto
 
+    
     #Digitar o número
     print(f"Você digitou o número: {entrada}")
+    print("\n")
 
     #Condicional de acerto ou erro
     if (acerto):
@@ -28,4 +34,5 @@ for rodada in range(1,tentativas +1):
         if(numero_menor):
             print("O número digitado foi MENOR do que o número secreto")
     rodada = rodada + 1
+print("\n")
 print("GAME OVER!!!")
